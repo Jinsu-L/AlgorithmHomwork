0fb2da5763d11a7b8330cb04b15401dd2fd1f726
Jinsu Lim <dla0830ld@gmail.com>
2017-11-22 00:08:58 +0900

    complete SequenceAlignment

--- HW08_SequenceAlignment/src/SequenceAlignment.java
+++ HW08_SequenceAlignment/src/SequenceAlignment.java
@@ -1,18 +1,4 @@
 import java.util.Scanner;
-/*
-input example 1 :
-string1 : ocurrance
-string2 : occurrence
-output :
-MIN COST : 3
-
-input example 2:
-string1 : CTGACCTACCT
-string2 : CCTGACTACAT
-output :
-MIN COST : 4
-
- */
 
 public class SequenceAlignment {
     private static final int GAP_PENALTY = 1; // δ
@@ -27,23 +13,36 @@ public class SequenceAlignment {
         String string1 = scan.nextLine();
         System.out.printf("string2 : ");
         String string2 = scan.nextLine();
-        int[][] opt = new int[string1.length()][string2.length()];
-
-        sequenceAlign(opt, string1, string2);
-        printOpt(opt);
+        int[][] opt = new int[string1.length() + 1][string2.length() + 1];
+        char[][] flowMap = new char[string1.length() + 1][string2.length() + 1];
+        sequenceAlign(opt, flowMap, string1, string2);
+        trackingRouteAndPrint(flowMap, opt, string1, string2);
     }
 
-    private static void sequenceAlign(int[][] opt, String string1, String string2) {
-        for (int i = 0; i < string2.length(); i++)
+    private static void sequenceAlign(int[][] opt, char[][] flowMap, String string1, String string2) {
+        for (int i = 0; i < string2.length() + 1; i++) {
             opt[0][i] = i * GAP_PENALTY;
+            flowMap[0][i] = LEFT;
+        }
+        for (int i = 0; i < string1.length() + 1; i++) {
+            opt[i][0] = i * GAP_PENALTY;
+            flowMap[i][0] = UP;
+        }
 
-        for (int i = 0; i < string1.length(); i++)
-            opt[i][0] = i * i * GAP_PENALTY;
+        for (int i = 1; i < string1.length() + 1; i++) {
+            for (int j = 1; j < string2.length() + 1; j++) {
+                opt[i][j] = GAP_PENALTY + opt[i - 1][j];
+                flowMap[i][j] = UP;
 
+                if (opt[i][j] > GAP_PENALTY + opt[i][j - 1]) {
+                    opt[i][j] = GAP_PENALTY + opt[i][j - 1];
+                    flowMap[i][j] = LEFT;
+                }
 
-        for (int i = 1; i < string1.length(); i++) {
-            for (int j = 1; j < string2.length(); j++) {
-                opt[i][j] = Math.min(calMismatchPenalty(string1, string2, i, j) + opt[i - 1][j - 1], Math.min(GAP_PENALTY + opt[i - 1][j], GAP_PENALTY + opt[i][j - 1]));
+                if (opt[i][j] > calMismatchPenalty(string1, string2, i - 1, j - 1) + opt[i - 1][j - 1]) {
+                    opt[i][j] = calMismatchPenalty(string1, string2, i - 1, j - 1) + opt[i - 1][j - 1];
+                    flowMap[i][j] = LEFTUPDIAGONAL;
+                }
             }
         }
     }
@@ -54,11 +53,55 @@ public class SequenceAlignment {
         return MISMATCH_PENALTY;
     }
 
-    private static void printOpt(int[][] opt) {
-        int m = opt.length - 1;
-        int n = opt[0].length - 1;
-        System.out.println("MIN COST : " + opt[m][n] + "\n");
+    private static void trackingRouteAndPrint(char[][] flowMap, int[][] opt, String string1, String string2) {
+        int start_i = flowMap.length - 1;
+        int start_j = flowMap[0].length - 1;
 
+        String[][] extendedArray = new String[opt.length * 2 + 1][opt[0].length * 2 + 1];
 
+        // extend from opt to extendedArray
+        for (int i = 0; i < extendedArray.length; i++) {
+            for (int j = 0; j < extendedArray[0].length; j++) {
+                if (i == 0 && j > 3 && j % 2 == 0)
+                    extendedArray[i][j] = String.valueOf(string2.charAt((j - 4) / 2));
+                else if (j == 0 && i > 3 && i % 2 == 0)
+                    extendedArray[i][j] = String.valueOf(string1.charAt((i - 4) / 2));
+                else if (i != 0 && i != 1 && j != 0 && j != 1 && i % 2 == 0 && j % 2 == 0)
+                    extendedArray[i][j] = String.valueOf(opt[(i - 2) / 2][(j - 2) / 2]);
+                else
+                    extendedArray[i][j] = "";
+            }
+        }
+
+        // tracking Route
+        int m = extendedArray.length - 1;
+        int n = extendedArray[0].length - 1;
+        int i = start_i;
+        int j = start_j;
+        while ((i != 0 || j != 0) && m > 1 && n > 1) {
+            if (flowMap[i][j] == UP) {
+                extendedArray[m - 1][n] = String.valueOf(UP);
+                m -= 2;
+                i--;
+            } else if (flowMap[i][j] == LEFTUPDIAGONAL) {
+                extendedArray[m - 1][n - 1] = String.valueOf(LEFTUPDIAGONAL);
+                m -= 2;
+                n -= 2;
+                i--;
+                j--;
+            } else if (flowMap[i][j] == LEFT) {
+                extendedArray[m][n - 1] = String.valueOf(LEFT);
+                n -= 2;
+                j--;
+            }
+        }
+
+        System.out.println("\nMIN COST : " + opt[opt.length - 1][opt[0].length - 1]);
+
+        for (String[] row : extendedArray) {
+            for (String item : row)
+                System.out.printf("%2s", item);
+            System.out.println("");
+        }
     }
 }
